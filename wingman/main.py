fa8797a620886b2f2ca40d89ef879c39e6f8cd8b
mhouse_impulse <michaelhousera@gmail.com>
2026-03-08 10:57:23 -0400

    add version details to main

--- wingman/main.py
+++ wingman/main.py
@@ -10,7 +10,8 @@ try:
 except Exception:
     keyboard_module = None
 
-WINGMAN_VERSION = "1.3.1"
+WINGMAN_VERSION = "1.4.0"
+WINGMAN_VERSION_DETAILS = "Automated flare deployment on missile incoming detection"
 # Key controls (change these to remap start/pause and cancel)
 EXIT_KEY = 'backspace'
 
